e1113c9198dec2b7950d11455f675580ef9bf493
Franco Ezequiel Marchegiani <francoezequielmarchegiani2019@gmail.com>
2020-11-24 14:25:02 -0300

    Error al llamar API Json

--- js/buscador.js
+++ js/buscador.js
@@ -1,5 +1,5 @@
 //const endpoint = '../discos.json';
-$("#boton__busqueda").click(function(e){
+/* $("#boton__busqueda").click(function(e){
     e.preventDefault()
     $.ajax({
         url: "../discos.json",
@@ -11,4 +11,13 @@ $("#boton__busqueda").click(function(e){
             });
         }
     });
-});
+}); */
+
+function buscarDisco(e){
+    e.preventDefault();
+    $.get("https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json",
+    function(data, status){
+        console.log(data);
+        alert("Resultado " + data + "\n Estado: " + status);
+    });
+}
